8be6b258cb7c3c18c50b54ed6a3adcea060de950
Aaron George <101379811+Aaronk41@users.noreply.github.com>
2025-08-04 11:58:44 -0400

    Delete src/precipitation.py

--- src/precipitation.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def get_total_precipitation_last_24h(df: pd.DataFrame, station_id: str) -> float:
-    from datetime import datetime, timedelta
-    now = datetime.now()
-    since = now - timedelta(hours=24)
-    filtered = df[(df['station_id'] == station_id) & (df['observation_time'] >= since)]
-    total_precip = filtered['precipitation'].sum()
-    return round(total_precip, 2)
